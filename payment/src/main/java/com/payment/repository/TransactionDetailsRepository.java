2f8fa6870d39d02c5867fc3936f9c4a61755cb77
Jensonmathew01 <Jenson.Mathew@ust.com>
2023-05-22 17:48:58 +0530

    Initial Commit: Dont Modify

---
+++ payment/src/main/java/com/payment/repository/TransactionDetailsRepository.java
@@ -0,0 +1,12 @@
+package com.payment.repository;
+
+import com.payment.entity.TransactionDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface TransactionDetailsRepository extends JpaRepository<TransactionDetails, Long> {
+
+    Optional<TransactionDetails> findByOrderId(long orderId);
+}
